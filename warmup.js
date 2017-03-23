bb29a833dd7daf375d8dc179598e7cd9f7bf8248
rukaroa <rukaroa@gmail.com>
2017-03-23 18:44:30 -0500

    clarification

--- warmup.js
+++ warmup.js
@@ -1,5 +1,7 @@
 var num1 = process.argv[2];
 var num2 = process.argv[3];
 
+console.log("Numbers are equal");
 console.log(num1 == num2);
+console.log("Both Divisible by 7");
 console.log(num1 % 7 == 0 && num2 % 7 == 0);
