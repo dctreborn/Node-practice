bf95631646dcea1086b67f46c9db85f15d597996
rukaroa <rukaroa@gmail.com>
2017-03-23 19:32:22 -0500

    git ignore

--- read-best-things.js
+++ read-best-things.js
@@ -1,13 +1,10 @@
 var fs = require("fs");
 
 fs.readFile("best_things_ever.txt","utf8", function(err, data){
-    console.log(data);
 
     var arrayData = data.split(",");
 
-    console.log(arrayData);
-
     for (var i = 0; i < arrayData.length; i++){
-        console.log(arrayData[i]);
+        console.log(arrayData[i].trim());
     }
 })
