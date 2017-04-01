04b87befa0d590424e549610605905443a473a05
rukaroa <rukaroa@gmail.com>
2017-04-01 14:04:46 -0500

    more callbacks

--- callback.js
+++ callback.js
@@ -1,14 +1,37 @@
-var holla = function(phrase) {
-    console.log(phrase + ", HOLLLLLAAAAA!");
+// var holla = function(phrase) {
+//     console.log(phrase + ", HOLLLLLAAAAA!");
+// }
+
+// var cheers = function(phrase) {
+//     console.log(phrase + ", cheers!");
+// }
+
+// function greet(method) {
+//     method("Hello, my friend.");
+// }
+
+// greet(holla);
+// greet(cheers);
+
+var fs = require("fs");
+
+function fun1(str, method) {
+    console.log(str);
+    method();
 }
 
-var cheers = function(phrase) {
-    console.log(phrase + ", cheers!");
+function fun2(bool, method) {
+    if (bool) {
+        method();
+    }
 }
 
-function greet(method) {
-    method("Hello, my friend.");
+function fun3(value, method) {
+    return function() {
+        return method(value);
+    }
 }
 
-greet(holla);
-greet(cheers);
+fs.writeFile('message.txt', "uses callback");
+
+fun1("hi", fun2(true, fun2(99, fun1(1))))
